12a87042f6dc0631cdacbc0d75849ae0392f628a
lirihuang <lirihuang@tal.com>
2024-11-26 22:44:22 +0800

    mod:json格式化

--- src/components/JSONParser.vue
+++ src/components/JSONParser.vue
@@ -1,35 +1,55 @@
 <template>
-  <div>
+  <div class="json-parser">
     <h2>JSON 解析</h2>
-    <textarea
-      v-model="jsonInput"
-      placeholder="输入 JSON 字符串"
-      :style="{ width: '100%', height: '200px' }"
-    ></textarea>
-    <button @click="formatJson">格式化 JSON</button>
-    <div v-if="error" style="color: red;">{{ error }}</div>
-    <pre v-if="formattedJson">{{ formattedJson }}</pre>
+    <div class="json-container">
+      <textarea
+        v-model="jsonInput"
+        placeholder="输入 JSON 字符串"
+        @input="formatJson"
+      ></textarea>
+      <div class="json-output">
+        <vue-json-pretty
+          :data="parsedJson"
+          :deep="1"
+          :showLineNumber="false"
+          :expandIcon="expandIcon"
+          :collapseIcon="collapseIcon"
+          :keyStyle="keyStyle"
+          :valueStyle="valueStyle"
+        />
+      </div>
+    </div>
+    <div v-if="error" class="error">{{ error }}</div>
   </div>
 </template>
 
 <script>
+import VueJsonPretty from 'vue-json-pretty';
+import 'vue-json-pretty/lib/styles.css';
+
 export default {
+  components: {
+    VueJsonPretty
+  },
   data() {
     return {
       jsonInput: '',
-      formattedJson: '',
-      error: ''
+      parsedJson: null,
+      error: '',
+      expandIcon: '<span style="color: skyblue;">+</span>',
+      collapseIcon: '<span style="color: lightcoral;">-</span>',
+      keyStyle: 'color: darkviolet;',
+      valueStyle: 'color: green;'
     };
   },
   methods: {
     formatJson() {
       try {
         this.error = '';
-        const parsedJson = JSON.parse(this.jsonInput);
-        this.formattedJson = JSON.stringify(parsedJson, null, 2);
+        this.parsedJson = JSON.parse(this.jsonInput);
       } catch (e) {
         this.error = '输入的字符串不是有效的 JSON 格式';
-        this.formattedJson = '';
+        this.parsedJson = null;
       }
     }
   }
@@ -37,9 +57,37 @@ export default {
 </script>
 
 <style scoped>
-textarea {
+.json-parser {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.json-container {
+  display: flex;
   width: 100%;
-  height: 200px;
-  margin-bottom: 10px;
+  max-width: 800px;
+}
+
+textarea {
+  width: 50%;
+  height: 400px;
+  margin-right: 20px;
+  padding: 10px;
+  box-sizing: border-box;
+}
+
+.json-output {
+  width: 50%;
+  height: 400px;
+  overflow-y: auto;
+  border: 1px solid #ccc;
+  padding: 10px;
+  box-sizing: border-box;
+}
+
+.error {
+  color: red;
+  margin-top: 10px;
 }
 </style>
